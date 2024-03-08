fbb0b79f946e5699e62c5a5bff711441f18f39dd
Gus <149393@coderacademy.edu.au>
2024-03-08 23:08:36 +1100

    Created CLI controller and laid out user table

--- models/user.py
+++ models/user.py
@@ -1,26 +1,25 @@
 from init import db, ma
 
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = "user_table"
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    
+    name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
-    date_joined = db.Column(db.Date)
-    
+    date_joined = db.Column(db.String, nullable=False)
     age = db.Column(db.String, nullable=False)
     weight = db.Column(db.String, nullable=False)
     height = db.Column(db.String, nullable=False)
-    gender = db.Column(db.Integer, nullable=False)
-    
+    gender = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     
 class UserSchema(ma.Schema):
     class Meta:
+        # fields = ("id", "name", "email", "password", "date_joined", "is_admin")
         fields = ("id", "name", "email", "password", "date_joined", "age", "weight", "height", "gender", "is_admin")
         
+
 user_schema = UserSchema(exclude=["password"])
 users_schema = UserSchema(many=True, exclude=["password"])
         
